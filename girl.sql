8e212750c185dac98be8752f94e8f191f88286c1
whatsyournameort <309516685@qq.com>
2018-11-08 18:20:46 +0800

    Add files via upload

---
+++ girl.sql
@@ -0,0 +1,6 @@
+INSERT INTO `girl` VALUES (1, '凤姐', 18, 'B');
+INSERT INTO `girl` VALUES (2, '芙蓉姐姐', 24, 'C');
+INSERT INTO `girl` VALUES (4, '苍老师', 12, 'B');
+INSERT INTO `girl` VALUES (5, '网王二麻子', 25, 'C');
+INSERT INTO `girl` VALUES (7, '凤姐', 28, 'A');
+INSERT INTO `girl` VALUES (8, '波多老师', 25, 'G');
