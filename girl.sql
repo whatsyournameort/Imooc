33bd03a8c97e1fe557c579a4ff9da8254ae10bec
whatsyournameort <309516685@qq.com>
2018-11-08 18:34:58 +0800

    Update girl.sql

--- girl.sql
+++ girl.sql
@@ -1,3 +1,12 @@
+CREATE TABLE `girl` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `cup_size` varchar(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+
 INSERT INTO `girl` VALUES (1, '凤姐', 18, 'B');
 INSERT INTO `girl` VALUES (2, '芙蓉姐姐', 24, 'C');
 INSERT INTO `girl` VALUES (4, '苍老师', 12, 'B');
